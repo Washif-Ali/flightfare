bb1c013513348630620cfa7958e39c17c72fe5f7
Washif-Ali <2400166a@student.tp.edu.sg>
2025-07-30 12:20:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -204,8 +204,8 @@ st.markdown(
     <style>
     .stApp {
         background: linear-gradient(
-            rgba(0, 0, 0, 0.4), 
-            rgba(0, 0, 0, 0.4)
+            rgba(0, 0, 0, 0.8), 
+            rgba(0, 0, 0, 0.8)
         ),
         url("https://images.unsplash.com/photo-1569154941061-e231b4725ef1?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         background-size: cover;
